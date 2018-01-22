8aa6ffbf996e492e0571d4a74494e2dd3ee31663
Justin Lorieau <justin@lorieau.com>
2018-01-22 17:22:57 -0600

    Move process_ast to ast.py

--- src/disseminate/parse.py
+++ src/disseminate/parse.py
@@ -1,40 +1,10 @@
 import regex
 
 #from attributes import parse_attributes
-from .tags import Tag
+from .ast import process_ast
 
 
 control_char = r'@'
-
-
-def process_ast(s):
-    """Parses a string into an AST comprising a list of lists with strings and
-    tags."""
-    ast = []
-    current_pos = 0
-    for m in regex.finditer(r'(@(?P<tag>[A-Za-z]\w*)'
-                            r'(?P<attributes>\[[^\]]+\])?'
-                            r'{(?P<content>(?>[^{}@]+|(?R))*)})', s):
-        # Find the match's start and end positions in the string
-        start, end = m.span()
-
-        # Add string up to this match
-        ast.append(s[current_pos:start])
-
-        # Reset the current position to the end of this match
-        current_pos = end
-
-        # Parse the match's content
-        d = m.groupdict()
-        tag_type = m['tag']
-        tag_content = process_ast(m['content'])
-        tag_attributes = m['attributes']
-        ast.append(Tag(tag_type, tag_content, tag_attributes))
-
-    # Add the end of the string
-    ast.append(s[current_pos:])
-
-    return ast
 
 
 class Processor(object):
