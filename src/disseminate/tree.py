85c60dcc17e7c85c42dad3670b2ff92709a39573
Justin Lorieau <justin@lorieau.com>
2018-01-23 11:37:10 -0600

    Refactored the loading of index files and managed directories in the Tree. Tests have been updated.

--- src/disseminate/tree.py
+++ src/disseminate/tree.py
@@ -10,6 +10,53 @@ from . import settings
 class TreeException(Exception): pass
 
 
+def load_index_files(index_path):
+    """Return a list of documents (source markup) files identified by an index
+    tree file (index.tree).
+
+    The search is recursive, in index.tree files in sub-directories are
+    listed.
+
+    Parameters
+    ----------
+    index_path: str
+        The path of an index tree file (index.tree).
+
+    Returns
+    -------
+    documents: list
+        An ordered list of documents (source markup) filenames and paths. These
+        respect the ordering in the tree index file given by index_path.
+    """
+    # Check that the index_path exists
+    if not os.path.exists(index_path):
+        msg = "The index tree file at '{}' cannot be found."
+        raise TreeException(msg.format(index_path))
+
+    # Load the files in the index_path
+    index_dir, index_filename = os.path.split(index_path)
+    with open(index_path) as f:
+        filenames = [i.strip() for i in f.readlines() if i.strip()]
+
+    # Prepend the index_dir to all filenames since the files listed in the
+    # tree index file are relative to the tree index path.
+    filepaths = [os.path.join(index_dir, i) for i in filenames]
+
+    documents = []
+
+    # If any of the files are tree index files (index.tree), then parse those
+    # as well
+    for filepath in filepaths:
+        path, filename = os.path.split(filepath)
+
+        if filename == settings.index_filename:
+            documents += load_index_files(filepath)
+        else:
+            documents.append(filepath)
+
+    return documents
+
+
 class Tree(object):
     """A tree of documents.
 
@@ -20,71 +67,186 @@ class Tree(object):
           available, these are used to construct the tree. The 'index.tree'
           contains a list of filenames, in order, for the source files to
           render.
+          If an index.tree file exists in a directory, it is assumed that this
+          file manages the tree index files and document files in its own
+          directory and all subdirectories.
         - If an 'index.tree' is not available, one is generated using the
-          default markup extension (see settings.document_extension). These
-          will be sorted by filename.
+          default document (markup) extension (see settings.document_extension).
+          These will be sorted by filename.
+
+    Attributes
+    ----------
+    subpath: str, optional
+        The subpath (sub-directory) of the current directory to search.
+    managed_dirs: dict of str
+        The subpaths (directories and subdirectories) managed by an index tree
+        file (index.tree).
+
+        ex: {'src/': 'src/index.tree'
+             'src/sub1': 'src/index.tree'
     """
 
-    def find_index_files(self, subpath=None):
-        """Locate index files (e.g. index.tree) in the current directory and
-        produce an index.
+    subpath = None
+    managed_dirs = None
+    document_files = None
+
+    def __init__(self, subpath=None):
+        self.subpath = subpath
+
+    def find_managed_dirs(self, subpath=None):
+        """Populate the managed directories (self.managed_dirs) by locate index
+        treefiles (e.g. index.tree).
+
+        .. note:: The presence of an index.tree file in a directory implies
+                  that the directory and all its subdirectories are managed by
+                  this file. Consequently, index tree files in sub-directories
+                  will be ignored, unless they are explicitly included in the
+                  root index tree file.
 
         Parameters
         ----------
         subpath: str, optional
-            If specified, only the given subpath will be searched for index
-            tree files.
+            If specified, only look in the given subpath directory. If this is not
+            specified, the value of self.subpath will be searched as well.
 
         Returns
         -------
-        index: dict
-            A dict with directories and subdirectories as the key and a list of
-            markup files (e.g. dsm) as the value.
-
-            ex: {'src/': ['src/intro.dsm', 'src/discussion.dsm'}
+        None
         """
+        self.managed_dirs = dict()
+        subpath = subpath if subpath is not None else self.subpath
+
+        # Construct the glob pattern to search for index.tree files
         if subpath:
             search_glob = os.path.join(subpath, '**', settings.index_filename)
         else:
             search_glob = os.path.join('**', settings.index_filename)
-        index = {}
 
-        for indexpath in glob.glob(search_glob, recursive=True):
-            # parse the paths
-            # indexpath: src/index.tree
-            # directory: src/
-            # filename: index.tree
-            directory, filename = os.path.split(indexpath)
+        # Get the glob list and sort it by length so that root paths come up
+        # first
+        index_paths = sorted(glob.glob(search_glob, recursive=True), key=len)
 
-            # Open the index file and verify that all files exist
-            with open(indexpath, 'r') as f:
-                markup_files = [i.strip() for i in f.readlines() if i.strip()]
+        # Go through each tree index file and mark its directory and
+        # sub-directories as managed
+        for index_path in index_paths:
+            index_dir, index_filename = os.path.split(index_path)
 
-            # These files are relative to the subdirectory. Create paths that
-            # are relative to the current directory.
-            markup_files = [os.path.join(directory, i) for i in markup_files]
+            # Do nothing if this directory is already managed by another
+            # index tree file.
+            if index_dir in self.managed_dirs:
+                continue
 
-            # Verify that all the files exist
-            for file in markup_files:
-                if not os.path.exists(file):
-                    msg = "The file '{}' in index tree '{}' does not exist."
-                    raise TreeException(msg.format(file, indexpath))
+            # Find all subdirectories to index_dir and mark these as managed
+            # as well. The os.path.split is designed to just isolate the
+            # directory name without the trailing '/' (and to make it
+            # compatible with managed paths calculated above)
+            glob_search = os.path.join(index_dir, '**/')
+            sub_dirs = [os.path.split(i)[0] for i in
+                        glob.glob(glob_search, recursive=True)]
 
-            # Verify that there are no duplicates
-            unique_files = set(markup_files)
-            if len(unique_files) < len(markup_files):  # Then there's a dup
-                seen = set()
-                for i in markup_files:
-                    if i in seen:
-                        msg = "The file '{}' in index tree '{}' is duplicated."
-                        raise TreeException(msg.format(i, indexpath))
-                    else:
-                        seen.add(i)
+            for sub_dir in sub_dirs:
+                self.managed_dirs[sub_dir] = index_path
 
-            # Add the entries to the index
-            index.update({directory:markup_files})
+        return None
 
-        return index
+    def find_index_files(self, subpath=None):
+        """Identify the index files and populate the document (markup) source
+        files in order.
+
+        .. note:: This function only looks at index.tree files that are in
+                  managed directories. See the :meth:`find_managed_dirs` above.
+
+        Parameters
+        ----------
+        subpath: str, optional
+            If specified, only look in the given subpath directory. If this is not
+            specified, the value of self.subpath will be searched as well.
+
+        Returns
+        -------
+        None
+        """
+        # Populate the managed_dirs
+        self.find_managed_dirs(subpath)
+
+        # Get a list of all index files. These are sorted by length so that
+        # root directories are presented first
+        index_files = set(self.managed_dirs.values)
+        index_files = sorted(index_files, key=len)
+
+        # Now create an index of document (markup) files. The index.tree
+        # files may themselves include index.tree files in subdirectories,
+        # so these have to be expanded
+
+
+    # def find_index_files(self, subpath=None):
+    #     """Locate index files (e.g. index.tree) in the current directory and
+    #     produce an index.
+    #
+    #     Parameters
+    #     ----------
+    #     subpath: str, optional
+    #         If specified, only the given subpath will be searched for index
+    #         tree files.
+    #
+    #     Returns
+    #     -------
+    #     index: list of typles
+    #         A list with tuples of the directories and document files.
+    #
+    #         ex: [('src', 'intro.dm'), ('src', 'discussion.dm')]
+    #     """
+    #     if subpath:
+    #         search_glob = os.path.join(subpath, '**', settings.index_filename)
+    #     else:
+    #         search_glob = os.path.join('**', settings.index_filename)
+    #     index = []
+    #
+    #     managed_paths = set()
+    #     for indexpath in glob.glob(search_glob, recursive=True):
+    #         # parse the paths
+    #         # indexpath: src/index.tree
+    #         # directory: src/
+    #         # filename: index.tree
+    #         directory, filename = os.path.split(indexpath)
+    #
+    #         # See if it's already in the managed paths so that sub-directories
+    #         # of this index are not added. If so, do nothing else
+    #         if any(i.startswith(directory) for i in managed_paths):
+    #             continue
+    #         managed_paths.append(directory)
+    #
+    #         # Open the index file and verify that all files exist
+    #         with open(indexpath, 'r') as f:
+    #             markup_files = [i.strip() for i in f.readlines() if i.strip()]
+    #
+    #         # These files are relative to the subdirectory. Create paths that
+    #         # are relative to the current directory.
+    #         markup_files = [os.path.join(directory, i) for i in markup_files]
+    #
+    #         # Verify that all the files exist
+    #         for file in markup_files:
+    #             if not os.path.exists(file):
+    #                 msg = "The file '{}' in index tree '{}' does not exist."
+    #                 raise TreeException(msg.format(file, indexpath))
+    #
+    #         # Verify that there are no duplicates
+    #         unique_files = set(markup_files)
+    #         if len(unique_files) < len(markup_files):  # Then there's a dup
+    #             seen = set()
+    #             for i in markup_files:
+    #                 if i in seen:
+    #                     msg = "The file '{}' in index tree '{}' is duplicated."
+    #                     raise TreeException(msg.format(i, indexpath))
+    #                 else:
+    #                     seen.add(i)
+    #
+    #
+    #         # Add the entries to the index
+    #         for file in markup_files:
+    #             index.append(os.path.split(file))
+    #
+    #     return index
 
     def find_document_files(self, subpath=None):
         """Locate documents (markup files) and produce an index.
@@ -125,7 +287,11 @@ class Tree(object):
 
         return index
 
-
+    #def merge_indexes(self, preferred_index, supplemental_index):
+    #    """Merges two indices.
+    #        .. note: If entries are marked in both indexes, then the
+    #             preferred_index takes precedence.
+    #    """
 
 
 
--- tests/tree/test_tree.py
+++ tests/tree/test_tree.py
@@ -4,69 +4,153 @@ Tests for the index tree
 import pytest
 
 from disseminate import Tree
-from disseminate.tree import TreeException
+from disseminate.tree import TreeException, load_index_files
 
 
-def test_basic_index():
-    """Tests a basic index tree file."""
-    tree = Tree()
-
+def test_find_managed_dirs():
+    """Tests that the managed directories are correctly found."""
     # The 'examples1' directory contains an index.tree in the 'examples1/sub1'
-    # directory
-    index = tree.find_index_files(subpath='tests/tree/examples1/')
+    # directory with two document files. Therefore the sub1 directory is
+    # considered managed
+    tree1 = Tree(subpath="tests/tree/examples1")
+    tree1.find_managed_dirs()
 
-    # The index should have one item because there's one index file.
-    assert len(index) == 1
+    assert isinstance(tree1.managed_dirs, dict)
+    assert len(tree1.managed_dirs) == 1
+    key1 = 'tests/tree/examples1/sub1'
+    assert key1 in tree1.managed_dirs
+    assert tree1.managed_dirs[key1] == 'tests/tree/examples1/sub1/index.tree'
 
-    # The index should have the index's file's directory as a key
-    assert 'tests/tree/examples1/sub1' in index
+    # The 'examples5' directory contains an index.tree file in the root
+    # directory (tests/tree/examples5) and an index.tree file in each of the
+    # 'sub1', 'sub2', 'sub2/subsub2' and 'sub3' sub-directories.
+    # However, the root index.tree file manages all of the sub-directories.
+    # The managed_dirs attribute should have 5 directories (root and 4
+    # sub-directories) all pointing to the same index.tree in the root
+    # directory.
+    tree5 = Tree(subpath="tests/tree/examples5")
+    tree5.find_managed_dirs()
 
-    # The value for this key should be the markup files in that directory,
-    # in order
-    assert (index['tests/tree/examples1/sub1'] ==
-            ['tests/tree/examples1/sub1/intro.dm',
-             'tests/tree/examples1/sub1/appendix.dm'])
+    assert isinstance(tree5.managed_dirs, dict)
+    assert len(tree5.managed_dirs) == 5
 
-
-def test_duplicate_index():
-    """Tests an index tree file with duplicate entries."""
-    tree = Tree()
-
-    # The 'examples2' directory contains an index.tree in the 'examples2/sub1'
-    # directory with duplicate 'intro.dm'
-    with pytest.raises(TreeException):
-        index = tree.find_index_files(subpath='tests/tree/examples2/')
-
-
-def test_missing_file():
-    """Tests an index tree file with a missing entry."""
-    tree = Tree()
-
-    # The 'examples3' directory contains an index.tree in the 'examples3/sub1'
-    # directory with a missing file 'missing.dm'
-    with pytest.raises(TreeException):
-        index = tree.find_index_files(subpath='tests/tree/examples3/')
+    index_file = 'tests/tree/examples5/index.tree'
+    for key in ('tests/tree/examples5',
+                'tests/tree/examples5/sub1',
+                'tests/tree/examples5/sub2',
+                'tests/tree/examples5/sub2/subsub2',
+                'tests/tree/examples5/sub3'):
+        assert key in tree5.managed_dirs
+        assert tree5.managed_dirs[key] == index_file
 
 
-def test_basic_document_search():
-    """Tests the find_document_files method."""
-    tree = Tree()
+def test_load_index_files():
+    """Tests that the tree index files are properly read."""
+    # The 'examples1' directory contains an index.tree in the 'examples1/sub1'
+    # directory with two document files.
+    documents = load_index_files('tests/tree/examples1/sub1/index.tree')
 
-    # The 'examples1' directory contains document (markup) files in the root
-    # directory and in the 'sub1' directory
-    index = tree.find_document_files(subpath='tests/tree/examples1')
+    # The documents list should have two items for the two document (markup
+    # source files) identified in the index.tree
+    assert len(documents) == 2
+    assert documents[0] == 'tests/tree/examples1/sub1/intro.dm'
+    assert documents[1] == 'tests/tree/examples1/sub1/appendix.dm'
 
-    # The 'examples1' directory has 3 document files. 1 in the root, and 2 in
-    # the 'sub1' sub-directory
-    assert len(index) == 2
+    # The 'examples5' directory contains an index.tree file in the root
+    # directory (tests/tree/examples5). This file includes a pointer to
+    # the index.tree files in 'sub1' and 'sub2' but not 'sub3'. The index.tree
+    # files point to a single document file in sub1, but it points to a document
+    # and an index.tree in sub2.
+    documents = load_index_files('tests/tree/examples5/index.tree')
 
-    key1 = 'tests/tree/examples1'
-    assert key1 in index
-    assert len(index[key1]) == 1
-    assert 'tests/tree/examples1/index.dm' in index[key1]
+    # The documents list should have four items: one index.dm in the project
+    # root, and one in each of 'sub1', 'sub2' and 'sub2/subsub2'.
+    # The sub2 file is listed before sub1
 
-    key2 = 'tests/tree/examples1/sub1'
-    assert key2 in index
-    assert len(index[key2]) == 2
-    assert 'tests/tree/examples1/sub1/intro.dm' in index[key2]
-    assert 'tests/tree/examples1/sub1/appendix.dm' in index[key2]
+    assert len(documents) == 4
+    assert documents[0] == 'tests/tree/examples5/index.dm'
+    assert documents[1] == 'tests/tree/examples5/sub2/index.dm'
+    assert documents[2] == 'tests/tree/examples5/sub2/subsub2/index.dm'
+    assert documents[3] == 'tests/tree/examples5/sub1/index.dm'
+
+
+#
+# def test_basic_index():
+#     """Tests a basic index tree file."""
+#     tree = Tree()
+#
+#     # The 'examples1' directory contains an index.tree in the 'examples1/sub1'
+#     # directory with two document files
+#     index = tree.find_index_files(subpath='tests/tree/examples1/')
+#
+#     # The index should have two items, one for each document in the index tree
+#     # file.
+#     assert len(index) == 2
+#
+#     # The index should have the index's file's directory as a key
+#     assert index[0] == ('tests/tree/examples1/sub1', 'intro.dm')
+#     assert index[1] == ('tests/tree/examples1/sub1', 'appendix.dm')
+#
+#
+# def test_duplicate_index():
+#     """Tests an index tree file with duplicate entries."""
+#     tree = Tree()
+#
+#     # The 'examples2' directory contains an index.tree in the 'examples2/sub1'
+#     # directory with duplicate 'intro.dm'
+#     with pytest.raises(TreeException):
+#         index = tree.find_index_files(subpath='tests/tree/examples2/')
+#
+#
+# def test_missing_file():
+#     """Tests an index tree file with a missing entry."""
+#     tree = Tree()
+#
+#     # The 'examples3' directory contains an index.tree in the 'examples3/sub1'
+#     # directory with a missing file 'missing.dm'
+#     with pytest.raises(TreeException):
+#         index = tree.find_index_files(subpath='tests/tree/examples3/')
+#
+#
+# def test_subdirectories_index():
+#     """Tests an index tree that refers to files in a subdirectory."""
+#     tree = Tree()
+#
+#     # The 'examples4' directory contains an index.tree in the 'examples4/sub1'
+#     # directory with 3 document files: 'intro.dm', 'appendix.dm' and
+#     # 'subsub1/intro.dm'
+#     index = tree.find_index_files(subpath='tests/tree/examples4/')
+#
+#     # The index should have three directory entries
+#     assert len(index) == 3
+#
+#     assert index[0] == ('tests/tree/examples4/sub1', 'intro.dm')
+#     assert index[1] == ('tests/tree/examples4/sub1', 'appendix.dm')
+#     assert index[2] == ('tests/tree/examples4/sub1/subsub1', 'intro.dm')
+#
+# def test_nested_indexes()
+#     """Tests index tree files with index tree files within them."""
+#
+#
+# def test_basic_document_search():
+#     """Tests the find_document_files method."""
+#     tree = Tree()
+#
+#     # The 'examples1' directory contains document (markup) files in the root
+#     # directory and in the 'sub1' directory
+#     index = tree.find_document_files(subpath='tests/tree/examples1')
+#
+#     # The 'examples1' directory has 3 document files. 1 in the root, and 2 in
+#     # the 'sub1' sub-directory
+#     assert len(index) == 2
+#
+#     key1 = 'tests/tree/examples1'
+#     assert key1 in index
+#     assert len(index[key1]) == 1
+#     assert 'tests/tree/examples1/index.dm' in index[key1]
+#
+#     key2 = 'tests/tree/examples1/sub1'
+#     assert key2 in index
+#     assert len(index[key2]) == 2
+#     assert 'tests/tree/examples1/sub1/intro.dm' in index[key2]
+#     assert 'tests/tree/examples1/sub1/appendix.dm' in index[key2]
