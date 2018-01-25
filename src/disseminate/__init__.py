ae54575583230c4d28dc69616dfb0a65d385e0d4
Justin Lorieau <justin@lorieau.com>
2018-01-25 08:19:25 -0600

    Updated default imports for package

--- src/disseminate/__init__.py
+++ src/disseminate/__init__.py
@@ -1,7 +1,5 @@
-from .tags import Tag, TagFactory
 from .ast import process_ast
 from .tree import Tree
-#from .attributes import parse_attributes
+from .document import Document
 
 from . import tags
-
