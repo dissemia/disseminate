6ef46c0162caa36c786c28628c2c44436c158fc1
Justin Lorieau <justin@lorieau.com>
2018-01-27 09:08:45 -0600

    Minor updates to templates

--- src/disseminate/templates/core.py
+++ src/disseminate/templates/core.py
@@ -12,7 +12,8 @@ environments = {}
 
 
 def get_template(src_filepath, target,
-                 template_basename=settings.default_basefilename):
+                 template_basename=settings.default_basefilename,
+                 module_only=False):
     """Fetch the best template for rendering a document.
 
     A template must implement a render method that takes kwargs to render
@@ -28,6 +29,9 @@ def get_template(src_filepath, target,
         The target extension to render. ex: ".html", ".tex"
     template_name : str, optional
         The base filename of the template file. ex: "template"
+    module_only : bool, optional
+        If specified, the template will be searched in disseminate's package
+        only (not in the source paths). By default, this option is False.
 
     Returns
     -------
@@ -37,24 +41,35 @@ def get_template(src_filepath, target,
         template.
     """
     # Get the top path and see if an environment has already been created
-    top_dir = os.path.dirname(src_filepath)
+    if module_only:
+        top_dir = '__module__'
+    else:
+        top_dir = os.path.dirname(src_filepath)
+
     if top_dir in environments:
         env = environments[top_dir]
     else:
-        # An environment hasn't been created yet. Make one.
-        # Create a jinja2 FileSystemLoader that checks the directory of
-        # src_filepath
-        # and all parent directories.
-        dir_tree = []
+        fsl=None
 
-        parent_dir = top_dir
-        while parent_dir != "":
-            dir_tree.append(parent_dir)
-            parent_dir = os.path.dirname(parent_dir)
+        if module_only:
+            # An environment hasn't been created yet. Make one.
+            # Create a jinja2 FileSystemLoader that checks the directory of
+            # src_filepath
+            # and all parent directories.
+            dir_tree = []
 
-        fsl = jinja2.FileSystemLoader(dir_tree)
+            parent_dir = top_dir
+            while parent_dir != "":
+                dir_tree.append(parent_dir)
+                parent_dir = os.path.dirname(parent_dir)
+
+            fsl = jinja2.FileSystemLoader(dir_tree)
         dl = jinja2.PackageLoader('disseminate', 'templates/template_files')
-        cl  = jinja2.ChoiceLoader([fsl, dl])
+
+        if fsl is not None:
+            cl = jinja2.ChoiceLoader([fsl, dl])
+        else:
+            cl = dl
 
         # Create the environment
         env = jinja2.Environment(loader=cl)
