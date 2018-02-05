7ec062f95632eb1144a67347700bd6509be45b81
Justin Lorieau <justin@lorieau.com>
2018-02-05 14:21:11 -0600

    Fixed mkdir_p to handle filepaths and directories

--- src/disseminate/utils/file.py
+++ src/disseminate/utils/file.py
@@ -5,15 +5,21 @@ import os
 import errno
 
 
-def mkdir_p(filepath):
-    """Creates directories for the given filepath, if needed.
+def mkdir_p(path):
+    """Creates directories for the given filepath or directory, if needed.
 
     Parameters
     ----------
-    filepath : str
-        A filepath that includes a directory *and* and filename.
+    path : str
+        A filepath or directory.
     """
-    base, file = os.path.split(filepath)
+    # Determine if it's a directory or a file
+    is_dir = os.path.splitext(path)[1] == ""
+    
+    if is_dir:
+        base = path
+    else:
+        base, _ = os.path.split(path)
     try:
         os.makedirs(base)
     except OSError as exc:
