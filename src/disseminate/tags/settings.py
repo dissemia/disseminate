281939afce959b1e7d846a0a472079932748f612
Justin Lorieau <justin@lorieau.com>
2018-02-05 19:24:04 -0600

    Cleaned up default HTML template page

--- src/disseminate/tags/settings.py
+++ src/disseminate/tags/settings.py
@@ -5,7 +5,7 @@
 #: ~~~~~~~~~
 
 #: The tag to use for the document in the HTML page
-html_root_tag = 'body'
+html_root_tag = 'root'
 
 #: Render HTML pages with newlines and indentation
 html_pretty = True
