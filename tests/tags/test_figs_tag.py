0b889178d7ba1d9f7491bddd24a1272dd58654d9
Justin Lorieau <justin@lorieau.com>
2018-03-08 06:55:13 -0600

    Added tests for the marginfig tag

--- tests/tags/test_figs_tag.py
+++ tests/tags/test_figs_tag.py
@@ -1,4 +1,18 @@
 """
 Tests for the figure tags.
 """
+from disseminate.ast import process_ast
 
+
+def test_marginfig_parsing():
+    """Test the parsing of marginfig tags."""
+
+    test1 = "@marginfig{fig1}"
+    root = process_ast(test1)
+    assert root.content.name == 'marginfig'
+    assert root.content.content == 'fig1'
+
+    test2 = "@marginfig{{fig1}}"
+    root = process_ast(test2)
+    assert root.content.name == 'marginfig'
+    assert root.content.content == '{fig1}'
