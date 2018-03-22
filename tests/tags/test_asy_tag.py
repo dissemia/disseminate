1d690c32db213b2e73824f0271fc5791f668a1dd
Justin Lorieau <justin@lorieau.com>
2018-03-22 13:52:25 -0500

    Refactored to merge local_context and global_context into a single context

--- tests/tags/test_asy_tag.py
+++ tests/tags/test_asy_tag.py
@@ -14,11 +14,11 @@ def test_asy_html(tmpdir):
     target_root = str(tmpdir)
 
     # First, we'll test the case when asy code is used directly in the tag.
-    # We will not use a document_src_filepath in the global_context, since
-    # when this is missing, it is used in generating the cached filename
+    # We will not use a document src_filepath in the context, since
+    # when this is missing, it is not used in generating the cached filename
 
-    # Setup the dependency manager in the global context. This is needed
-    # to find and convert images by the img tag.
+    # Setup the dependency manager in the context. This is needed to find and
+    # convert images by the img tag.
     dep = DependencyManager(project_root=project_root,
                             target_root=target_root)
     context = {'dependency_manager': dep,
@@ -53,14 +53,15 @@ def test_asy_html(tmpdir):
 
     # Second, we'll test the case when asy code is used directly in the tag.
     # We will now use a src_filepath of the markup document in the
-    # local_context, since it is not used in generating the cached filename
+    # context, since it will be used in generating the cached filename
 
-    # Setup the dependency manager in the global context. This is needed
+    # Setup the dependency manager in the context. This is needed
     # to find and convert images by the img tag. We'll place the project root
     # in 'src'
     dep = DependencyManager(project_root=project_root,
                             target_root=target_root)
     context = {'dependency_manager': dep,
+               'src_filepath': 'src/chapter/test.dm',
                'project_root': 'src',
                'target_root': target_root}
 
@@ -76,9 +77,10 @@ def test_asy_html(tmpdir):
 
     # Check the rendered tag and that the asy and svg files were properly
     # created
+    print(root_html)
     assert root_html == '<img src="/media/chapter/test_69a34c39e1.svg"/>'
     assert tmpdir.ensure(settings.media_dir, 'chapter', 'test_69a34c39e1.svg')
-    assert tmpdir.ensure('.html', settings.media_dir, 'cahpter',
+    assert tmpdir.ensure('.html', settings.media_dir, 'chapter',
                          'test_69a34c39e1.svg')
 
 
@@ -91,18 +93,16 @@ def test_asy_html_attribute(tmpdir):
     target_root = str(tmpdir)
 
     # First, we'll test the case when asy code is used directly in the tag.
-    # We will not use a document_src_filepath in the global_context, since
-    # when this is missing, it is used in generating the cached filename
+    # We will not use a document's src_filepath in the global_context, since
+    # when this is missing, it is not used in generating the cached filename
 
     # Setup the dependency manager in the global context. This is needed
     # to find and convert images by the img tag.
     dep = DependencyManager(project_root=project_root,
-                            target_root=target_root,
-                            segregate_targets=True)
-    global_context = {'_dependencies': dep,
-                      '_project_root': project_root,
-                      '_target_root': target_root}
-    local_context = {'_targets': {'.html': 'test.html'}}
+                            target_root=target_root)
+    context = {'dependency_manager': dep,
+               'project_root': project_root,
+               'target_root': target_root}
 
     # Generate the markup
     src = """@asy[scale=2.0]{
@@ -111,8 +111,7 @@ def test_asy_html_attribute(tmpdir):
         draw(unitcircle);  }"""
 
     # Generate a tag and compare the generated tex to the answer key
-    root = process_ast(src, local_context=local_context,
-                       global_context=global_context)
+    root = process_ast(src, context=context)
 
     # The following root tags have to be stripped for the html strings
     root_start = '<span class="root">\n  '
