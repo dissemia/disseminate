eb3d0b67fa676631188874a2d8d8ed5b91dc16e4
J Lorieau <justin@lorieau.com>
2019-08-21 08:40:35 -0500

    Filter documents to necessarily be a root_document from load_root_documents

--- tests/document/test_document_utils.py
+++ tests/document/test_document_utils.py
@@ -7,8 +7,8 @@ from disseminate.paths import SourcePath
 from disseminate.document.utils import (find_project_paths,
                                         find_root_src_filepaths,
                                         load_root_documents)
-from disseminate.document.document_context import DocumentContext
 from disseminate.utils.tests import strip_leading_space
+
 
 def test_find_project_paths():
     """Test the find_project_paths function."""
@@ -83,7 +83,7 @@ def test_find_project_paths():
             not in project_paths)
 
 
-def _check_src_filepaths(src_filepaths):
+def _check_src_filepaths(src_filepaths, only_root=False):
     """Tests for src_filepaths from the tests directory"""
     # tests/document/example1/
     # ├── dummy.dm
@@ -122,7 +122,11 @@ def _check_src_filepaths(src_filepaths):
     #     ├── file1.dm  (includes file2.dm)
     #     └── file2.dm
     assert Path('tests/document/example6/src/file1.dm') in src_filepaths
-    assert Path('tests/document/example6/src/file2.dm') in src_filepaths
+
+    if only_root:
+        assert Path('tests/document/example6/src/file2.dm') not in src_filepaths
+    else:
+        assert Path('tests/document/example6/src/file2.dm') in src_filepaths
 
     # tests/document/example7/
     # └── src
@@ -148,8 +152,9 @@ def test_find_root_src_filepaths():
     _check_src_filepaths(src_filepaths)
 
 
-def test_load_root_documents(tmpdir):
-    """Test the load_root_documents function"""
+def test_load_root_documents_src_filepaths(tmpdir):
+    """Test the load_root_documents function and the src_filepaths
+    of root documents."""
 
     # 1. load the root documents
     documents = load_root_documents('tests/document')
@@ -158,7 +163,7 @@ def test_load_root_documents(tmpdir):
     src_filepaths = [d.src_filepath for d in documents]
 
     # Check the src_filepaths
-    _check_src_filepaths(src_filepaths)
+    _check_src_filepaths(src_filepaths, only_root=True)
 
     # 2. Create a srcfile in the tmpdir, render it and check the paths
     src_path = SourcePath(tmpdir, 'src')
@@ -183,3 +188,97 @@ def test_load_root_documents(tmpdir):
     doc.render()
     target_filepath = tmpdir / 'html' / 'test.html'
     assert target_filepath.exists()
+
+
+def test_load_root_documents_project_target_roots(tmpdir):
+    """Test the load_root_documents function and the project_roots and
+    target_roots of root documents."""
+
+    # 1. load the root documents
+    documents = load_root_documents('tests/document')
+
+    # Get the src_filepaths and project_roots for these documents
+    src_filepaths = [d.src_filepath for d in documents]
+    project_roots = [src_filepath.project_root
+                     for src_filepath in src_filepaths]
+    target_roots = [doc.target_root for doc in documents]
+
+    # tests/document/example1/
+    # ├── dummy.dm
+    # ├── dummy.html
+    # └── dummy.tex
+    assert Path('tests/document/example1') in project_roots
+    assert Path('tests/document/example1') in target_roots
+
+
+    # tests/document/example2/
+    # ├── noheader.dm
+    # └── withheader.dm
+    assert Path('tests/document/example2') in project_roots
+    assert Path('tests/document/example2') in target_roots
+
+    # tests/document/example4/
+    # └── src
+    #     └── file.dm
+    assert Path('tests/document/example4/src') in project_roots
+    assert Path('tests/document/example4') in target_roots
+
+    # tests/document/example5/
+    # ├── index.dm
+    # ├── sub1
+    # │   └── index.dm
+    # ├── sub2
+    # │   ├── index.dm
+    # │   └── subsub2
+    # │       └── index.dm
+    # └── sub3
+    #     └── index.dm
+    assert Path('tests/document/example5') in project_roots
+    assert Path('tests/document/example5/sub1') not in project_roots
+    assert Path('tests/document/example5/sub2') not in project_roots
+    assert Path('tests/document/example5/sub3') not in project_roots
+    assert Path('tests/document/example5') in target_roots
+
+    # tests/document/example6/
+    # └── src
+    #     ├── file1.dm  (includes file2.dm)
+    #     └── file2.dm
+    assert Path('tests/document/example6/src') in project_roots
+    assert Path('tests/document/example6') in target_roots
+
+    # tests/document/example7/
+    # └── src
+    #     ├── file1.dm
+    #     └── sub1
+    #         ├── file11.dm
+    #         └── subsub1
+    #             └── file111.dm
+    assert Path('tests/document/example7/src') in project_roots
+    assert (Path('tests/document/example7/src/sub1')
+            not in src_filepaths)
+    assert (Path('tests/document/example7/src/sub1/subsub1')
+            not in src_filepaths)
+    assert Path('tests/document/example7') in target_roots
+
+
+def test_load_root_documents_specific_path():
+    """Test the load_root_documents function using the specific path for a
+    root document"""
+
+    # Use example7
+    # tests/document/example7/
+    # └── src
+    #     ├── file1.dm
+    #     └── sub1
+    #         ├── file11.dm
+    #         └── subsub1
+    #             └── file111.dm
+
+    documents = load_root_documents('tests/document/example7/src/file1.dm')
+    assert len(documents) == 1
+    assert (documents[0].src_filepath ==
+            Path('tests/document/example7/src/file1.dm'))
+    assert (documents[0].project_root ==
+            Path('tests/document/example7/src'))
+    assert (documents[0].target_root ==
+            Path('tests/document/example7'))
