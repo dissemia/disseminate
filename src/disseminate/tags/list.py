ab717a86db1142b6d1ddf7da2875e795369257ee
J Lorieau <justin@lorieau.com>
2019-10-16 06:12:25 -0500

    Initial implementation of @list tag

---
+++ src/disseminate/tags/list.py
@@ -0,0 +1,166 @@
+"""
+Tags for lists.
+"""
+import regex
+
+from .tag import Tag, TagError
+
+
+# Find lists with entries labeled with a '-' or '*' bullet for each list item
+re_list = regex.compile(r'^\s*[\*\-]', regex.MULTILINE)
+
+# Regex to clean list items with multi-line line-items
+re_multiline_items = regex.compile(r'\s*\n+\s+')
+
+
+def parse_string_list(s):
+    """Parse a string with lists.
+
+    Parameters
+    ----------
+    s : str
+        The string to parse
+
+    Returns
+    -------
+    parsed_list : List[Tuple[int, str]]
+
+    Examples
+    --------
+    >>> parse_string_list("- This is my first item.\\n"
+    ...                   "  - This is my first subitem\\n")
+    [(0, 'This is my first item.'), (2, 'This is my first subitem')]
+    """
+    parsed_list = []
+    current_match = None
+    current_pos = 0
+    list_level = 1
+
+    for match in re_list.finditer(s):
+        # Skip the first entry, since this is the string contents before
+        # the start of the list
+        if current_match is None:
+            current_match = match
+            current_pos = match.end()
+
+            # Find the level. The number of spaces determines the level
+            list_level = match.group().count(' ')
+            continue
+
+        # Retrieve the string up to this point. Remove leading spaces.
+        end_pos = match.start()
+        sub_string = s[current_pos:end_pos].strip()
+
+        # Create the new entry in the returned list
+        parsed_list.append((list_level, sub_string))
+
+        # Set the current position and current match for the next iteration
+        current_match = match
+        current_pos = match.end()
+        list_level = match.group().count(' ')
+
+    # Add the remaining match to the returned list
+    if current_match is not None:
+        # Retrieve the string up to this point up until the end.
+        # Remove leading spaces.
+        end_pos = current_match.end()
+        sub_string = s[end_pos:].strip()
+
+        # Create the new entry in the returned list
+        parsed_list.append((list_level, sub_string))
+
+    return parsed_list
+
+
+def clean_string_list(parsed_list):
+    """Clean the string list created by parse_string_list.
+
+    Cleaning include removeing extra spaces and newlines in parse string line
+    elements.
+    
+    Parameters
+    ----------
+    parsed_list : List[Tuple[int, str]]
+        The parsed list from parse_string_list.
+        
+    Returned
+    --------
+    cleaned_list : List[Tuple[int, str]]
+        The cleaned list.
+
+    >>> l = parse_string_list("- This is my first item.\\n"
+    ...                   "  - This is my first subitem\\n")
+    >>> clean_string_list(l)
+    [(0, 'This is my first item.'), (2, 'This is my first subitem')]
+    """
+    return [(level, re_multiline_items.sub(" ", s))
+            if isinstance(s, str) else
+            (level, s)
+            for level, s in parsed_list ]
+
+
+def parse_list(content, context):
+    """Parse lists (and sublists) from a string or list of content.
+
+    Parameters
+    ----------
+    content : Union[str, List[Union[str, list, :obj:`Tag \
+        <disseminate.tags.tag.Tag>`], :obj:`Tag <disseminate.tags.tag.Tag>`]
+        The contents of the tag. It can either be a string, a tag or a list
+        of strings, tags and lists.
+    context : :obj:`.document.DocumentContext`
+        The document's context dict.
+
+    Returns
+    -------
+    returned_list : list
+        The list of parse list items, to be used for the content of a list.
+    """
+    # Unify the format of the cotent
+    if isinstance(content, str):
+        content = [content]
+    elif isinstance(content, Tag):
+        content.content = parse_list(content.content)
+    elif isinstance(content, list):
+        pass
+    else:
+        return []
+
+    # Go item-by-item to find list items
+    returned_list = []
+    current_list = None
+
+    for item in content:
+        # Add non-strings to a list
+        if not isinstance(item, str) and current_list is not None:
+            # If it's not a string, just add it to the current list
+            current_list.append(item)
+
+        # If it's a string, see if it has list items in it
+        parsed_list = parse_string_list(item)
+        parsed_list = clean_string_list(parsed_list)
+
+        if parse_list:
+            for level, list_item in parsed_list:
+                if current_list is not None:
+                    current_list.append(list_item)
+                else:
+                    current_list = [list_item]
+
+                # Create the list item and add it the returned list
+                content = (current_list[0] if len(current_list) == 1 else
+                           current_list)
+                li = ListItem(name='listitem', content=content,
+                              attributes='level={}'.format(level),
+                              context=context)
+                returned_list.append(li)
+
+                # Reset the current_list
+                current_list = []
+
+    return returned_list
+
+
+class ListItem(Tag):
+
+    active = False
---
+++ tests/tags/test_list.py
@@ -0,0 +1,128 @@
+"""
+Test the @list tag.
+"""
+from disseminate.tags.list import (parse_list, parse_string_list,
+                                   clean_string_list, ListItem)
+
+
+def test_parse_string_list():
+    """Test the parse_string_list and clean_string_list functions."""
+
+    # 1. A non-nested list
+    t1 = """
+    - One
+    - Two
+    - Three
+    """
+    l1 = parse_string_list(t1)
+    c1 = clean_string_list(l1)
+
+    assert l1 == [(4, 'One'), (4, 'Two'), (4, 'Three')]
+    assert c1 == [(4, 'One'), (4, 'Two'), (4, 'Three')]
+
+    # 2. A nested list
+    t2 = """
+    - One
+      - Two with
+        multiple lines
+    - Three
+    """
+    l2 = parse_string_list(t2)
+    c2 = clean_string_list(l2)
+    assert (l2 ==
+            [(4, 'One'), (6, 'Two with\n        multiple lines'),
+             (4, 'Three')])
+    assert (c2 ==
+            [(4, 'One'), (6, 'Two with multiple lines'),
+             (4, 'Three')])
+
+    # 3. An empty string
+    t3 = "This is a simple string"
+    l3 = parse_string_list(t3)
+    c3 = clean_string_list(l3)
+    assert l3 == []
+    assert c3 == []
+
+
+def test_parse_list_strings(doc):
+    """Test the parse_list_strings function"""
+
+    # 1. Test a simple list string
+    t1 = """
+    - One
+    - Two
+    - Three
+    """
+    returned_list = parse_list(content=t1, context=doc.context)
+
+    assert len(returned_list) == 3
+
+    assert isinstance(returned_list[0], ListItem)
+    assert returned_list[0].name == "listitem"
+    assert returned_list[0].content == "One"
+    assert returned_list[0].attributes['level'] == '4'
+
+    assert isinstance(returned_list[1], ListItem)
+    assert returned_list[1].name == "listitem"
+    assert returned_list[1].content == "Two"
+    assert returned_list[1].attributes['level'] == '4'
+
+    assert isinstance(returned_list[2], ListItem)
+    assert returned_list[2].name == "listitem"
+    assert returned_list[2].content == "Three"
+    assert returned_list[2].attributes['level'] == '4'
+
+    # 2. Test a nested list
+    t2 = """
+    - One
+      - Two with
+        multiple lines
+    - Three
+    """
+    returned_list = parse_list(content=t2, context=doc.context)
+
+    assert len(returned_list) == 3
+
+    assert isinstance(returned_list[0], ListItem)
+    assert returned_list[0].name == "listitem"
+    assert returned_list[0].content == "One"
+    assert returned_list[0].attributes['level'] == '4'
+
+    assert isinstance(returned_list[1], ListItem)
+    assert returned_list[1].name == "listitem"
+    assert returned_list[1].content == "Two with multiple lines"
+    assert returned_list[1].attributes['level'] == '6'
+
+    assert isinstance(returned_list[2], ListItem)
+    assert returned_list[2].name == "listitem"
+    assert returned_list[2].content == "Three"
+    assert returned_list[2].attributes['level'] == '4'
+
+    # 3. Test a list with tags
+    t3 = """
+    - One
+      - Two with
+        @b{multiple} lines
+    - Three
+    """
+    returned_list = parse_list(content=t3, context=doc.context)
+
+    assert len(returned_list) == 3
+
+    assert isinstance(returned_list[0], ListItem)
+    assert returned_list[0].name == "listitem"
+    assert returned_list[0].content == "One"
+    assert returned_list[0].attributes['level'] == '4'
+
+    assert isinstance(returned_list[1], ListItem)
+    assert returned_list[1].name == "listitem"
+    assert returned_list[1].content[0] == "Two with "
+    assert returned_list[1].content[1].name == "b"
+    assert returned_list[1].content[1].content == "multiple"
+    assert returned_list[1].content[2] == " lines"
+    assert returned_list[1].attributes['level'] == '6'
+
+    assert isinstance(returned_list[2], ListItem)
+    assert returned_list[2].name == "listitem"
+    assert returned_list[2].content == "Three"
+    assert returned_list[2].attributes['level'] == '4'
