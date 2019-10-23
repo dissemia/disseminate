47fc3179e93f68945a16123f0e9bc1f578ef3cc5
J Lorieau <justin@lorieau.com>
2019-10-23 14:01:10 -0500

    Fixed bug in document deletion

--- src/disseminate/document/receivers/reset.py
+++ src/disseminate/document/receivers/reset.py
@@ -5,7 +5,6 @@ from ..signals import document_onload, document_deleted
 
 
 @document_onload.connect_via(order=100)
-@document_deleted.connect_via(order=100)
 def reset_document(document, **kwargs):
     """Reset the context and managers for a document on load."""
     context = getattr(document, 'context', dict())
@@ -25,5 +24,30 @@ def reset_document(document, **kwargs):
 
     # Reset the context
     context.reset()
+
+    return document
+
+
+@document_deleted.connect_via(order=100)
+def delete_document(document, **kwargs):
+    """Reset the context and managers for a document on load."""
+    context = getattr(document, 'context', dict())
+    src_filepath = context.get('src_filepath', None)
+
+    # Reset the dependency manager
+    if 'dependency_manager' in context and src_filepath is not None:
+        dep = context['dependency_manager']
+        dep.reset(src_filepath=document.src_filepath)
+
+    # Reset the label manager
+    if 'label_manager' in context and src_filepath is not None:
+        label_manager = context['label_manager']
+
+        # Reset the labels for this document.
+        label_manager.reset(context=context)
+
+    # Reset the context
+    del context
+    delattr(document, 'context')
 
     return document
