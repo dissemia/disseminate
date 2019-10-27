29a77b9df92bddeb975f2a17cb9ec02a63079ba8
J Lorieau <justin@lorieau.com>
2019-10-27 12:30:31 -0500

    Eliminate the processors submodule and implement signals listing view

--- src/disseminate/server/views/__init__.py
+++ src/disseminate/server/views/__init__.py
@@ -1,6 +1,6 @@
 from .blueprints import system, tree, page
 from .tree import render_tree
 from .checker import render_checkers
-from .processors import render_processors
+from .signals import render_signals
 from .page import render_page
 from .exceptions import handle_500
