64ccef0ec7b83a5d312ddc3366bde9d7a5e1766f
J Lorieau <justin@lorieau.com>
2020-02-13 11:13:24 -0600

    Partial refactor of equation tag to work with refactored RenderedImg

--- src/disseminate/tags/eqs.py
+++ src/disseminate/tags/eqs.py
@@ -34,6 +34,7 @@ class Eq(RenderedImg):
 
     aliases = ('term', 'termb')
     active = True
+    input_format = '.tex'
 
     process_content = False
 
@@ -48,47 +49,22 @@ class Eq(RenderedImg):
 
     def __init__(self, name, content, attributes, context,
                  block_equation=False):
-        assert (context.is_valid('renderers') and
-                'equation' in context['renderers'])
         self.block_equation = block_equation
 
-        # Ensure the attributes argument is an Attributes type
-        attributes = Attributes(attributes)
-
-        # Get various properties from the attributes
-        env = attributes.get('env', target='.tex')
-        self.block_equation = (True if env is not None or
-                               block_equation else False)
-        self.tex_env = env if env is not None else self.default_block_env
-
-        # Prepare the tag to only have its contents processed
-        self.context = context
-        self.content = content
-        self.attributes = attributes
-
-        # Replace macros and process content
-        process_macros(tag=self)
-        process_content(tag=self, tag_factory=TagFactory)
-
-        # Take the content, and convert it to latex. Save the tex_content
-        # separately because the contents of this tag will be converted to
-        # an image path by the parent class.
-        tex_content = content_to_str(self.content, target='.tex', mathmode=True)
-        tex_content = tex_content.strip(' \t\n')
-        self.tex_content = tex_content
-        content = self.tex
-
-        # Crop equation images created by the dependency manager. This removes
-        # white space around the image so that the equation images.
-        # Note: This crop command should not cut off baselines such that
-        # equation images won't line up properly with the surrounding text.
-        # ex: H vs Hy
-        attributes['crop'] = (100, 100, 0, 0)
+        # Convert the content into a string
+        content = content_to_str(content, target='.tex',
+                                 mathmode=True)
+        content = content.strip(' \t\n')
 
         super().__init__(name=name, content=content, attributes=attributes,
-                         context=context, render_target='.tex')
+                         context=context)
+        #
+        # # Get various properties from the attributes
+        # env = attributes.get('env', target='.tex')
+        # self.block_equation = (True if env is not None orself.block_equation else False)
+        # self.tex_env = env if env is not None else self.default_block_env
 
-    def render_content(self, content, context):
+    def prepare_content(self, content=None, context=None):
         """Render the content in LaTeX into a valid .tex file."""
         # Get the renderer for the equation
         if ('renderers' not in context or
@@ -96,17 +72,26 @@ class Eq(RenderedImg):
             raise TagError("Missing equation renderer in the document context")
         renderer = context['renderers']['equation']
 
+        # Get the unspecified arguments
+        content = content if content is not None else self.tex
+        context = context if context is not None else context
+
         # Make a copy of the context and add the content as the 'body' entry.
         context_cp = copy(context)
         context_cp['body'] = content
-
-        # Get the kwargs and args to pass to the template
-        kwargs = self.template_kwargs()
+        kwargs = dict()
 
         return renderer.render(context=context_cp, target='.tex', **kwargs)
 
     def html_fmt(self, content=None, attributes=None, level=1):
         attrs = attributes if attributes is not None else self.attributes.copy()
+
+        # Crop equation images created by the dependency manager. This removes
+        # white space around the image so that the equation images.
+        # Note: This crop command should not cut off baselines such that
+        # equation images won't line up properly with the surrounding text.
+        # ex: H vs Hy
+        attrs['crop'] = (100, 100, 0, 0)
 
         if self.block_equation:
             attrs['class'] = 'eq blockeq'
@@ -116,12 +101,15 @@ class Eq(RenderedImg):
                                         level=level)
 
     def tex_fmt(self, content=None, attributes=None, mathmode=False, level=1):
-        if content is None:
-            content = (self.tex_content if self.tex_content is not None
-                       else self.content)
+        # Retrieve unspecified arguments
+        attributes = attributes if attributes is not None else self.attributes
+        content = content if content is not None else self.content
+
+        # Determine the environment
+        env = attributes.get('env', target='.tex') or self.default_block_env
+        block_equation = self.block_equation or self.paragraph_role == 'block'
 
         # Add bold and color if specified
-        attributes = attributes if attributes is not None else self.attributes
         if 'color' in attributes:
             content = tex_cmd(cmd='textcolor',
                               attributes=attributes['color'],
@@ -135,8 +123,8 @@ class Eq(RenderedImg):
         if mathmode:
             return content
         else:
-            if self.block_equation or self.paragraph_role == 'block':
-                return tex_env(env=self.tex_env, attributes=attributes,
+            if block_equation:
+                return tex_env(env=env, attributes=attributes,
                                formatted_content=content, min_newlines=True)
             else:
                 return tex_cmd(cmd='ensuremath', attributes=attributes,
