172edaf898a25ecbbe835b88d5627f3ecd69262b
J Lorieau <justin@lorieau.com>
2020-03-09 07:57:26 -0500

    HtmlBuilder: Add a test for inherited templates

---
+++ tests/builders/target_builders/example2/dummy.dm
@@ -0,0 +1,7 @@
+---
+targets: html, tex
+template: books/tufte
+---
+@i{This is my example}
+
+Here is @b{my} example file.
--- tests/builders/target_builders/test_html_builder.py
+++ tests/builders/target_builders/test_html_builder.py
@@ -62,37 +62,39 @@ def test_html_builder_simple(setup_example):
     assert builder.status == 'done'
 
 
-#
-#
-# def test_html_builder_simple(env):
-#     """Test simple builds with the HtmlBuilder"""
-#     assert False
-#
-#
-# def test_html_builder_paths(env):
-#     """Test builds with different paths for infilepaths"""
-#
-#     # Create an html builder
-#     html_builder = HtmlBuilder(env)
-#
-#     # Add build
-#     html_builder.add_build(infilepath='', outfilepath='', context=context)
-#
-#
-# def test_html_builder_document(env):
-#     """Test access to the HtmlBuilder from the document context"""
-#     context = env.context
-#     env = context['environment']
-#
-#     # Get the HtmlBuilder
-#     html_builder = env.get_target_builder('.html')
-#
-#     # Load document, check if build is needed
-#
-#     # Add builds (Tags)
-#     environment = context['environment']
-#     html_builder = environment.get_builder(document_target='.html',
-#                                            context=context)
-#     html_builder.add_build(infilepaths='', outfilepath='')
-#
-#     # Write html file (document)
+def test_html_builder_inherited(setup_example):
+    """Test a build with the HtmlBuilder using an inherited template."""
+    # 1. example 1: tests/builders/target_builders/example1
+    env, doc = setup_example('tests/builders/target_builders/example2',
+                             'dummy.dm')
+
+    # Setup the builder
+    builder = HtmlBuilder(env, document=doc)
+
+    # check the build
+    assert builder.build_needed()
+    assert builder.status == 'ready'
+    assert not doc.targets['.html'].exists()
+
+    # Try the build
+    assert builder.build(complete=True) == 'done'
+    assert builder.status == 'done'
+    assert doc.targets['.html'].exists()
+    print(doc.targets['.html'])
+    # Check the answer key
+    key = pathlib.Path('tests/builders/target_builders/example2/dummy.html')
+    assert doc.targets['.html'].read_text() == key.read_text()
+
+    # Check the copied files
+    assert TargetPath(target_root=doc.target_root, target='html',
+                      subpath='media/css/bootstrap.min.css').exists()
+    assert TargetPath(target_root=doc.target_root, target='html',
+                      subpath='media/css/base.css').exists()
+    assert TargetPath(target_root=doc.target_root, target='html',
+                      subpath='media/css/pygments.css').exists()
+    assert TargetPath(target_root=doc.target_root, target='html',
+                      subpath='media/css/tufte.css').exists()
+
+    # New builders don't need to rebuild.
+    builder = HtmlBuilder(env, document=doc)
+    assert builder.status == 'done'
