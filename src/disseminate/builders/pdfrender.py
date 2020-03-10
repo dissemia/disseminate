935c1e9fe452e207282df45df555dfbc7ddaae2a
J Lorieau <justin@lorieau.com>
2020-03-10 17:28:04 -0500

    Split PdfRender from Pdflatex

---
+++ src/disseminate/builders/pdfrender.py
@@ -0,0 +1,41 @@
+"""
+A builder to render a tex file to pdf.
+"""
+from .composite_builders import SequentialBuilder
+from .pdflatex import Pdflatex
+from .jinja_render import JinjaRender
+
+
+class PdfRender(SequentialBuilder):
+    """Render a tex file and render the pdf."""
+
+    available = True
+
+    def __init__(self, env, context=None, template=None, infilepaths=None,
+                 outfilepath=None, subbuilders=None, **kwargs):
+
+        # Setup the arguments
+        subbuilders = subbuilders or []
+
+        # Setup a render if no infilepath is specified
+        if infilepaths is None and context is not None:
+            # If no infilepaths are specified, we need to render one from
+            # the context
+            render_build = JinjaRender(env, context=context, target='.tex',
+                                       template=template, **kwargs)
+            subbuilders.append(render_build)
+
+            # Set the infilepath for this builder to match the render_build, if
+            # used, so that the Md5Decision is properly calculated
+            infilepaths = render_build.infilepaths
+
+        # Setup a (draftmode) Pdflatex builder
+        # pdf_draft_build = PdflatexDraft(env, **kwargs)
+        # subbuilders.append(pdf_draft_build)
+
+        # Setup a Pdflatex builder
+        pdf_build = Pdflatex(env, **kwargs)
+        subbuilders.append(pdf_build)
+
+        super().__init__(env, infilepaths=infilepaths, outfilepath=outfilepath,
+                         subbuilders=subbuilders, **kwargs)
--- tests/builders/test_pdflatex.py
+++ tests/builders/test_pdflatex.py
@@ -1,9 +1,7 @@
 """
-Test the pdflatex builder.
+Test the Pdflatex builder.
 """
-from collections import namedtuple
-
-from disseminate.builders.pdflatex import Pdflatex, PdfRender
+from disseminate.builders.pdflatex import Pdflatex
 from disseminate.paths import SourcePath, TargetPath
 
 
@@ -75,78 +73,3 @@ def test_pdflatex_simple(env):
     # Check the state of the build
     assert pdflatex.status == 'done'
     assert pdflatex.outfilepath.exists()
-
-
-def test_pdfrender_setup(env):
-    """Test the setup of the PdfRender builder."""
-    target_root = env.context['target_root']
-    context = env.context
-
-    # Create a mock tag and template
-    Tag = namedtuple("Tag", "tex")
-    context['body'] = Tag(tex='My test body')
-
-    # 1. Setup a build with an outfilepath
-    outfilepath = TargetPath(target_root=target_root,
-                             subpath='final.pdf')
-    pdfrender = PdfRender(env=env, context=context, outfilepath=outfilepath)
-
-    # Test the paths for the subbuilders
-    assert len(pdfrender.subbuilders) == 3
-
-    assert pdfrender.subbuilders[0].__class__.__name__ == 'JinjaRender'
-    assert len(pdfrender.subbuilders[0].infilepaths) == 3
-    assert (str(pdfrender.subbuilders[0].outfilepath.subpath) ==
-            'a635d8caba43.tex')
-
-    assert pdfrender.subbuilders[1].__class__.__name__ == 'Pdflatex'
-    assert (pdfrender.subbuilders[1].infilepaths[0] ==
-            pdfrender.subbuilders[0].outfilepath)
-    assert (str(pdfrender.subbuilders[1].outfilepath.subpath) ==
-            'a635d8caba43.pdf')
-
-    assert pdfrender.subbuilders[2].__class__.__name__ == 'Copy'
-    assert (pdfrender.subbuilders[2].infilepaths[0] ==
-            pdfrender.subbuilders[1].outfilepath)
-    assert pdfrender.subbuilders[2].outfilepath == outfilepath
-
-    assert len(pdfrender.infilepaths) == 3
-    assert pdfrender.outfilepath == outfilepath
-
-
-def test_pdfrender_simple(env):
-    """Test a simple build with the PdfRender builder."""
-    target_root = env.context['target_root']
-    context = env.context
-
-    # Create a mock tag and template
-    Tag = namedtuple("Tag", "tex")
-    context['body'] = Tag(tex='My test body')
-
-    # 1. Test a build with an outfilepath
-    outfilepath = TargetPath(target_root=target_root,
-                             subpath='final.pdf')
-    pdfrender = PdfRender(env=env, context=context, outfilepath=outfilepath)
-
-    # Check the builder status
-    assert not outfilepath.exists()
-    assert pdfrender.status == 'ready'
-
-    # Run the build
-    assert pdfrender.build(complete=True) == 'done'
-    assert pdfrender.status == 'done'
-    assert outfilepath.exists()
-
-    # A new builder shouldn't need a build
-    pdfrender = PdfRender(env=env, context=context, outfilepath=outfilepath)
-    assert pdfrender.status == 'done'
-
-    # 2. Changing the render contents should trigger a new build
-    context['body'] = Tag(tex='My new test body')
-    pdfrender = PdfRender(env=env, context=context, outfilepath=outfilepath)
-    assert pdfrender.status == 'ready'
-
-
-    # 2. Test a build without an outfilepath
-
-
